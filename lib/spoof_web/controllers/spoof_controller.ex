a16540225d0644a11a96672abd1122df774e7109
TORIFUKUKaiou <torifuku.kaiou@gmail.com>
2019-07-26 13:32:27 +0900

    customize for challecara.org

--- lib/spoof_web/controllers/spoof_controller.ex
+++ lib/spoof_web/controllers/spoof_controller.ex
@@ -26,6 +26,54 @@ defmodule SpoofWeb.SpoofController do
     send_resp(conn, 200, "")
   end
 
+  def create(conn, %{"text" => text, "command" => "/camel", "team_domain" => team_domain, "channel_name" => "directmessage", "user_name" => user_name}) do
+    spawn(SpoofWeb.SpoofController, :camel, [text, team_domain, "@#{user_name}"])
+
+    send_resp(conn, 200, "")
+  end
+
+  def create(conn, %{"text" => text, "command" => "/camel", "team_domain" => team_domain, "channel_name" => channel_name}) do
+    spawn(SpoofWeb.SpoofController, :camel, [text, team_domain, channel_name])
+
+    send_resp(conn, 200, "")
+  end
+
+  def create(conn, %{"text" => text, "command" => "/boy", "team_domain" => team_domain, "channel_name" => "directmessage", "user_name" => user_name}) do
+    spawn(SpoofWeb.SpoofController, :boy, [text, team_domain, "@#{user_name}"])
+
+    send_resp(conn, 200, "")
+  end
+
+  def create(conn, %{"text" => text, "command" => "/boy", "team_domain" => team_domain, "channel_name" => channel_name}) do
+    spawn(SpoofWeb.SpoofController, :boy, [text, team_domain, channel_name])
+
+    send_resp(conn, 200, "")
+  end
+
+  def create(conn, %{"text" => text, "command" => "/girl", "team_domain" => team_domain, "channel_name" => "directmessage", "user_name" => user_name}) do
+    spawn(SpoofWeb.SpoofController, :girl, [text, team_domain, "@#{user_name}"])
+
+    send_resp(conn, 200, "")
+  end
+
+  def create(conn, %{"text" => text, "command" => "/girl", "team_domain" => team_domain, "channel_name" => channel_name}) do
+    spawn(SpoofWeb.SpoofController, :girl, [text, team_domain, channel_name])
+
+    send_resp(conn, 200, "")
+  end
+
+  def create(conn, %{"text" => text, "command" => "/mentor", "team_domain" => team_domain, "channel_name" => "directmessage", "user_name" => user_name}) do
+    spawn(SpoofWeb.SpoofController, :mentor, [text, team_domain, "@#{user_name}"])
+
+    send_resp(conn, 200, "")
+  end
+
+  def create(conn, %{"text" => text, "command" => "/mentor", "team_domain" => team_domain, "channel_name" => channel_name}) do
+    spawn(SpoofWeb.SpoofController, :mentor, [text, team_domain, channel_name])
+
+    send_resp(conn, 200, "")
+  end
+
   def spoof(text, team_domain, channel_name) do
     [head | tail] = String.split(text, " ")
     msg = Enum.join(tail, " ")
@@ -40,6 +88,30 @@ defmodule SpoofWeb.SpoofController do
     domain(team_domain)
       |> webhook_url
       |> post(text, "ハナミトリ夫", "https://i.imgur.com/q58ZDDo.png", channel_name)
+  end
+
+  def camel(text, team_domain, channel_name) do
+    domain(team_domain)
+      |> webhook_url
+      |> post(text, "camel", "https://emoji.slack-edge.com/T1PM95NMT/challecara-camel/884b714ec87aa85b.png", channel_name)
+  end
+
+  def boy(text, team_domain, channel_name) do
+    domain(team_domain)
+      |> webhook_url
+      |> post(text, "boy", "https://emoji.slack-edge.com/T1PM95NMT/challecara-boy/223a09c8f4532f94.png", channel_name)
+  end
+
+  def girl(text, team_domain, channel_name) do
+    domain(team_domain)
+      |> webhook_url
+      |> post(text, "girl", "https://emoji.slack-edge.com/T1PM95NMT/challecara-girl/76bb1c3b336f522e.png", channel_name)
+  end
+
+  def mentor(text, team_domain, channel_name) do
+    domain(team_domain)
+      |> webhook_url
+      |> post(text, "mentor", "https://emoji.slack-edge.com/T1PM95NMT/challecara-mentor/be961fd29d7bacdd.png", channel_name)
   end
 
   defp post(url, text, username, icon_url, channel_name) do
